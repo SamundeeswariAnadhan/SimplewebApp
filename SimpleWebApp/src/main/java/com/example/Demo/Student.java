f4951e856f801b688f24cb41a548efa794148fc1
SamundeeswariAnadhan <91953293+SamundeeswariAnadhan@users.noreply.github.com>
2022-02-11 18:50:15 +0530

    Add files via upload

---
+++ SimpleWebApp/src/main/java/com/example/Demo/Student.java
@@ -0,0 +1,28 @@
+package com.example.Demo;
+
+public class Student
+{
+private int Sid;
+private String Sname;
+private String Dep;
+public int getSid() {
+	return Sid;
+}
+public void setSid(int sid) {
+	Sid = sid;
+}
+public String getSname() {
+	return Sname;
+}
+public void setSname(String sname) {
+	Sname = sname;
+}
+public String getDep() {
+	return Dep;
+}
+public void setDep(String dep) {
+	Dep = dep;
+}
+
+
+}
---
+++ SimpleWebApp/src/main/webapp/pages/home.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+        First web project for application flow
+       <br>
+        Welcome ${obj.sid},${obj.sname },${obj.dep } 
+</body>
+</html>
+
